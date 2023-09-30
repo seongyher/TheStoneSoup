8cdce6f3e5546302f78490de1bbfe7163ec5d1e9
aloxbii <abu94@uclive.ac.nz>
2023-09-30 18:50:08 +1300

    Calculation functionality started

---
+++ URP Game Jam Base/Assets/Scenes/CalculationsScene/DetectionZoneController.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Scripting;
+
+public class DetectionZoneController : MonoBehaviour
+{
+    [Tooltip("Exposed to editor for debugging purposes.")]
+    [SerializeField] private List<FoodItem> FoodList = null;
+
+    [Tooltip("Tick if the customer request requires you to avoid certain flavours. If this is ticked and the FlavoursToAvoid list is empty, the player will fail every time.")]
+    [SerializeField] private bool UsingFlavoursToAvoid = false;
+
+    [Tooltip("Flavours required to exist in pot. Duplicate instances of tags can be added for customer requests requiring a lot of a certain flavour.")]
+    [SerializeField] private List<string> FlavourRequirements = null;
+
+    [Tooltip("Flavours to avoid. Duplicate instances of tags can be added if you want to give the player an allowance for bad flavours. E.g. adding 2 \"spicy\" tags will only fail the player once two spicy items are present.")]
+    [SerializeField] private List<string> FlavoursToAvoid = null;
+
+    [SerializeField] private TextMeshProUGUI RequirementsTMP = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FoodList = new List<FoodItem>();
+        RequirementsTMP.SetText(string.Join(" ", FlavourRequirements));
+        RequirementsTMP.color = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FoodList.Add(collision.GetComponent<FoodItem>());
+        CheckRequirements();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        FoodItem foodItem = collision.GetComponent<FoodItem>();
+        if (foodItem != null)
+        {
+            if (FoodList.Contains(foodItem))
+            {
+                FoodList.Remove(foodItem);
+            }
+
+            CheckRequirements();
+        }
+    }
+
+    private void CheckRequirements()
+    {
+        List<string> RequirementsToCheck = new List<string>(FlavourRequirements);
+        List<string> AvoidedFoodToCheck = new List<string>(FlavoursToAvoid);
+
+        foreach (FoodItem foodItem in FoodList)
+        {
+            foreach (string flavour in foodItem.flavours)
+            {
+                if (RequirementsToCheck.Contains(flavour))
+                {
+                    RequirementsToCheck.Remove(flavour);
+                }
+            }
+        }
+
+        foreach (FoodItem foodItem in FoodList)
+        {
+            foreach (string flavour in foodItem.flavours)
+            {
+                if (AvoidedFoodToCheck.Contains(flavour))
+                {
+                    AvoidedFoodToCheck.Remove(flavour);
+                }
+            }
+        }
+
+        if (RequirementsToCheck.Count == 0 && (!UsingFlavoursToAvoid || AvoidedFoodToCheck.Count > 0))
+        {
+            // Customer requirements met
+            RequirementsTMP.color = Color.green;
+        }
+        else
+        {
+            // Customer request requirements not met
+            RequirementsTMP.color = Color.white;
+        }
+    }
+
+}
---
+++ URP Game Jam Base/Assets/Scenes/CalculationsScene/FoodItem.cs
@@ -0,0 +1,22 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodItem : MonoBehaviour
+{
+    [SerializeField] private List<string> Flavours = null;
+    public List<string> flavours => Flavours;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
